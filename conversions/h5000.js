fcee472961a194e763db789bb25b6406474bd4a8
Hans Tool <hans@defect.org>
2021-10-03 20:24:53 +0200

    Bla

--- conversions/h5000.js
+++ conversions/h5000.js
@@ -3,6 +3,7 @@ module.exports = (app, plugin) => {
     title: 'B&G Performance(130824)',
     optionKey: 'Performance_B&G',
     keys: ["performance.tackTrue", "performance.polarSpeed", "performance.polarSpeedRatio", "performance.velocityMadeGood", "navigation.leewayAngle", "performance.beatAngle"],
+    // timeouts: [200, 200, 200, 200, 200, 200],
     callback: (opp_target,
 	       			 // opp_cog,
                polar_speed,
@@ -20,20 +21,21 @@ module.exports = (app, plugin) => {
           {
             pgn: 130824,
 					  SID: 0x1e,
+            'Manufacturer Code': 'B&G',
             'Key 30': '8499',
-						'Opposite Tack Target Heading': opp_target,
+						'Opposite tack target heading': opp_target ? opp_target : 65535,
             'Key 31': '8318',
-            'Polar Speed': polar_speed,
+            'Polar Speed': polar_speed ? polar_speed : 65535,
             'Key 32': '8316',
-            'Polar Performance': polar_perf,
+            'Polar Performance': polar_perf ? polar_perf : 65535,
             'Key 33': '8477',
-            'VMG Performance': vmg_perf,
+            'VMG Performance': vmg_perf ? vmg_perf : 65535,
             'Key 34': '8319',
-            'Velocity Made Good': vmg,
+            'Velocity Made Good': vmg ? vmg_perf : 65535,
             'Key 35': '8322',
-            'Leeway Angle': leeway_angle,
+            'Leeway Angle': leeway_angle ? leeway_angle : 65535,
             'Key 36': '8458',
-            'Optimum Wind Angle': beat_angle
+            'Optimum Wind Angle': beat_angle ? beat_angle : 65535
           }
         ]
       } catch ( err ) {
